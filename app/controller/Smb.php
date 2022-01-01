ed4527f6df42d75dbfba6a59773e8b20e641c048
Piotr1103 <manjudamin@gmail.com>
2022-01-02 01:54:40 +0800

    實作插入方法

--- app/controller/Smb.php
+++ app/controller/Smb.php
@@ -5,6 +5,8 @@ namespace app\controller;
 
 use think\Request;
 use app\model\Smb as SmbModel;
+use app\validate\Smb as SmbValidate;
+use think\exception\ValidateException;
 
 class Smb
 {
@@ -47,7 +49,26 @@ class Smb
 	 */
 	public function save(Request $request)
 	{
-		//
+		//插入資料庫
+		$data = request()->param();
+
+		try{
+			validate(SmbValidate::class)->batch(true)->check($data);
+		}catch(ValidateException $exception){
+			return view('public/toast', [
+				'infos' => $exception->getError(),
+				'url_text' => '返回添加',
+				'url_path' => url('/smb/create'),
+			]);
+		}
+
+		$id = SmbModel::create($data)->getData('id');
+
+		return $id ? view('public/toast', [
+			'infos' 	=> ['恭喜，插入成功！'],
+			'url_text' 	=> '返回閱覽',
+			'url_path' 	=> url('/smb'),
+		]) : '插入失敗！';
 	}
 
 	/**
