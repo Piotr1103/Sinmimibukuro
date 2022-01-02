f687af4911de0747fd12c2797640504b5d3e236b
Piotr1103 <manjudamin@gmail.com>
2022-01-02 19:28:07 +0800

    定義登入路由和中間件

--- route/app.php
+++ route/app.php
@@ -17,3 +17,24 @@ Route::get('think', function () {
 Route::get('hello/:name', 'index/hello');
 
 Route::resource('smb', 'Smb');
+
+Route::group(function(){
+	Route::get('login', 'Login/index')->middleware(function($request, \Closure $next){
+		if(session('admin')){
+			return redirect('/');
+		}
+		return $next($request);
+	});
+	Route::post('login_check', 'Login/check');
+	Route::get('logout', 'Login/out');
+});
+
+Route::group(function(){
+	Route::get('create_smb', 'Smb/create');
+})->middleware(function($request, \Closure $next){
+	if(!session('?admin')){
+		return redirect('/login');
+	}
+
+	return $next($request);
+});
