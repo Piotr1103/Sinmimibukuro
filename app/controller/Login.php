7d1c4f1afa1457518485626f3b6c8001bc487032
Piotr1103 <manjudamin@gmail.com>
2022-01-02 19:26:54 +0800

    製作登入系統和畫面

---
+++ app/controller/Login.php
@@ -0,0 +1,48 @@
+<?php
+namespace app\controller;
+
+use think\Request;
+use think\facade\Validate;
+
+class Login
+{
+	public function index()
+	{
+		return view('index');
+	}
+
+	public function out()
+	{
+		session('admin', null);
+	}
+
+	public function check(Request $request)
+	{
+		$errors = [];
+		$data = $request->param();
+
+		$validate = Validate::rule([
+			'name|用戶名' => 'unique:auth,name^password',
+		]);
+
+		$result = $validate->batch(true)->check([
+			'name' 		=> $data['name'],
+			'password' 	=> sha1($data['password']),
+		]);
+
+		if(!captcha_check($data['captcha'])){
+			$errors[] = '驗證碼錯誤！';
+		}
+
+		if($result){
+			$errors[] = '帳戶或密碼不匹配';
+		}
+
+		if(!empty($errors)){
+			return '登錄失敗';
+		}else{
+			session('admin', $data['name']);
+			return redirect('/');
+		}
+	}
+}
