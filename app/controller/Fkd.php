05eeaf473deb1fc1523d4c3af69fbf7804f1f2bc
Piotr1103 <manjudamin@gmail.com>
2022-01-05 00:19:11 +0800

    舊怪談系統

--- app/controller/Fkd.php
+++ app/controller/Fkd.php
@@ -4,6 +4,10 @@ declare (strict_types = 1);
 namespace app\controller;
 
 use think\Request;
+use app\model\Fkd as FkdModel;
+use app\validate\Fkd as FkdValidate;
+use think\exception\ValidateException;
+use think\facade\Db;
 
 class Fkd
 {
@@ -15,6 +19,12 @@ class Fkd
 	public function index()
 	{
 		//
+		$list = FkdModel::order('tid')->paginate(5);
+
+		return view('index', [
+			'list' => $list,
+			'page' => request()->param('page'),
+		]);
 	}
 
 	/**
@@ -24,7 +34,9 @@ class Fkd
 	 */
 	public function create()
 	{
-		//
+		//tid是資料庫中找到的最大序數
+		$tid = Db::name('fkd')->max('tid');
+		return view('create', ['tid'=>$tid]);
 	}
 
 	/**
@@ -36,6 +48,26 @@ class Fkd
 	public function save(Request $request)
 	{
 		//
+		$data = request()->param();
+
+		try{
+			validate(FkdValidate::class)->batch(true)->check($data);
+		}catch(ValidateException $exception){
+			return view('public/toast', [
+				'infos' => $exception->getError(),
+				'url_text' => '返回添加',
+				//帶上page參數以便在toast可以有依據回到原先的頁面
+				'url_path' => url('/fkd/create', ['page'=>request()->param('page')]),
+			]);
+		}
+
+		$id = FkdModel::create($data)->getData('id');
+
+		return $id ? view('public/toast', [
+			'infos' => ['恭喜，插入成功！'],
+			'url_text' => '返回閱覽',
+			'url_path' => url('/fkd', ['page'=>request()->param('page')]),
+		]) : '插入失敗！';
 	}
 
 	/**
@@ -58,6 +90,9 @@ class Fkd
 	public function edit($id)
 	{
 		//
+		return view('edit', [
+			'obj' => FkdModel::find($id),
+		]);
 	}
 
 	/**
@@ -70,6 +105,26 @@ class Fkd
 	public function update(Request $request, $id)
 	{
 		//
+		$data = $request->param();
+
+		try{
+			validate(FkdValidate::class)->batch(true)->check($data);
+		}catch(ValidateException $exception){
+			return view('public/toast', [
+				'infos' 	=> $exception->getError(),
+				'url_text' 	=> '繼續修改',
+				//帶上page參數以便在toast可以有依據回到原先的頁面
+				'url_path' 	=> url('/fkd/'.$id.'/edit', ['page'=>request()->param('page')]),
+			]);
+		}
+
+		$id = FkdModel::update($data)->getData('id');
+
+		return $id ? view('public/toast', [
+			'infos' => ['恭喜，修改成功！'],
+			'url_text' => '返回閱覽',
+			'url_path' => url('/fkd', ['page'=>request()->param('page')]),
+		]) : '修改失敗！';
 	}
 
 	/**
--- app/model/Fkd.php
+++ app/model/Fkd.php
@@ -4,4 +4,24 @@ namespace app\model;
 use think\Model;
 
 class Fkd extends Model
-{}
+{
+	public function searchStatusAttr($query, $value)
+	{
+		return $value ? $query->where('status', $value) : '';
+	}
+
+	public function getStatusAttr($value)
+	{
+		$status = [
+			0 => '待審核',
+			1 => '已公開',
+		];
+
+		return $status[$value];
+	}
+
+	public function getBadgeAttr($value, $data)
+	{
+		return $data['status'] ? 'success' : 'warning';
+	}
+}
